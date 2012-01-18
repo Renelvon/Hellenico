b4374397ddef88b1047019e56c12f8567e3415d1
Nick Korasidis <renelvon@gmail.com>
2012-01-18 07:07:35 +0200

    lamps fix final

--- ch2/u2.1/ex2/main.cpp
+++ ch2/u2.1/ex2/main.cpp
@@ -88,7 +88,10 @@ int main() {
         if (off[0] > 0 && lamps[off[0]] == '1') isValid = false;
         if (off[1] > 0 && lamps[off[1]] == '1') isValid = false;
     
-        if (C < 3 && opcount != C) isValid = false;
+        if ((C == 0 && opcount > 0) ||
+            (C == 1 && opcount != 1)||
+            (C == 2 && !(opcount == 0 || opcount == 2))
+           ) isValid = false;
         if (isValid) {
             temp.assign(lamps + 1, lamps + N + 1);
             sols.push_back(temp);
