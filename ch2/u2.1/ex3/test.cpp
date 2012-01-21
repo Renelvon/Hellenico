7cdd31d198ca16fa306aa3be18041ef2a52c3a37
Nick Korasidis <renelvon@gmail.com>
2012-01-21 16:27:53 +0200

    clocks (matrix trick)

--- ch2/u2.1/ex3/test.cpp
+++ ch2/u2.1/ex3/test.cpp
@@ -1,8 +1,11 @@
+#include <algorithm>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 
 using namespace std;
+
+bool isZero (unsigned int i) { return i == 0; }
 
 int main() {
     ifstream inFile ("clocks.in");
@@ -36,13 +39,18 @@ int main() {
      */
 
     unsigned int clocks[numClocks];
+    fill(clocks, clocks + numClocks, 0);
+    /*
     for (unsigned int i = 0, temp; i < numClocks; ++i) {
         inFile >> temp;
         clocks[i] = (4 - temp) % 4;
     }
+    */
     
-    unsigned int sol[numOps];
+    unsigned int sol[numOps], solL[numOps * 3 + 1];
+    fill(solL, solL + numOps * 3 + 1, 0);
 
+    for (unsigned int k = 0, temp; k < 4 * 4 * 4 * 4 * 4 * 4 * 4 * 4 * 4; ++k) {
     for (unsigned int i = 0, sum; i < numOps; ++i) {
         sum = 0;
         for (unsigned int j = 0; j < numClocks; ++j) {
@@ -51,16 +59,35 @@ int main() {
         sol[i] = sum % 4;
     }
 
-    bool flag = false;
+
+    temp = 0;
     for (unsigned int i = 0; i < numOps; ++i) {
         for (unsigned int j = 0; j < sol[i]; ++j) {
-            if (flag) {
-                outFile << " ";
-            }
-            outFile << i + 1;
-            flag = true;
+            temp++;
         }
     }
+    solL[temp]++;
+    
+    for (unsigned int l = 0; l < numClocks; l++) {
+        if (clocks[l] < 3) {
+            clocks[l]++;
+            break;
+        } else {
+            clocks[l] = 0;
+        }
+    }
+    }
+    /*
+    for (unsigned int i = 0; i < numOps; ++i) {
+        for (unsigned int j = 0; j < sol[i]; ++j) {
+            outFile << i + 1 << " ";
+        }
+    }
+    */
+    for (unsigned int i = 0; i < numOps * 3 + 1; i++) {
+        cout << "Solutions with " << i << " moves: " << solL[i] << endl;
+    }
+    outFile << endl;
     outFile << endl;
 
     inFile.close();
